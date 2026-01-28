1207cff9f38330e12d115eb20783f836dab83e32
DGorla <dgorla@gmail.com>
2026-01-27 19:34:33 -0800

    fix: remove merge conflict markers in next.config.js

--- tanacara/next.config.js
+++ tanacara/next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-feature/estrutura-tanacara
-module.exports = {
-  reactStrictMode: true
-};
-=======
 const nextConfig = {
   reactStrictMode: true
 };
 
 module.exports = nextConfig;
-main
