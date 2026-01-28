a072e36f80b694cd1a22797fa0778ef33f70243f
DGorla <dgorla@gmail.com>
2026-01-27 19:44:42 -0800

    fix: remove merge conflict markers and simplify _app.tsx
    Removed unnecessary feature branch comments and cleaned up code.

--- tanacara/pages/_app.tsx
+++ tanacara/pages/_app.tsx
@@ -1,15 +1,6 @@
-feature/estrutura-tanacara
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-=======
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-main
